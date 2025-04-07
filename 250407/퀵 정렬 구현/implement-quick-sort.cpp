69e55d0d23e417a1a92db4e83fd29959974dcf79
Kim Donghyeok <109220011+Kim-DongHyeok@users.noreply.github.com>
2025-04-07 15:13:52 +0900

    Title: 퀵 정렬 구현, Time: 1026ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 250407/퀵 정렬 구현/implement-quick-sort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100000];
+
+void swap(int* a, int* b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void sort(int arr[], int start, int end) {
+    if (start >= end) return;
+
+    int pivot = arr[start];
+    int left = start + 1;
+    int right = end;
+
+    while (left <= right) {
+        while (left <= end && arr[left] < pivot) left++;
+        while (right > start && arr[right] > pivot) right--;
+
+        if (left < right) {
+            swap(&arr[left], &arr[right]);
+        }
+        else swap(&arr[right], &arr[start]);
+    }
+
+
+    sort(arr, start, right - 1);
+    sort(arr, right + 1, end);
+}
+
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, 0, n-1);
+
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    
+
+    return 0;
+}
+
