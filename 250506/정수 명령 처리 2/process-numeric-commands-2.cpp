19b5ff076c368c3e3e52b8ddf77c499e1e477724
Kim Donghyeok <109220011+Kim-DongHyeok@users.noreply.github.com>
2025-05-06 16:12:57 +0900

    Title: 정수 명령 처리 2, Time: 17ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250506/정수 명령 처리 2/process-numeric-commands-2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int N;
+string command[10000];
+int value[10000];
+
+int main() {
+    cin >> N;
+
+    queue<int> s;
+
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push") {
+            cin >> value[i];
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (command[i] == "push") {
+            s.push(value[i]);
+        }
+        else if (command[i] == "pop") {
+            cout << s.front() << endl;
+            s.pop();
+        }
+        else if (command[i] == "size") {
+            cout << s.size() << endl;
+        }
+        else if (command[i] == "empty") {
+            cout << (int)s.empty() << endl;
+        }
+        else if (command[i] == "front") {
+            cout << s.front() << endl;
+        }
+    }
+
+    return 0;
+}
