a381831ff8c1cf40acacdb8da53d8a8190bf5491
Kim Donghyeok <109220011+Kim-DongHyeok@users.noreply.github.com>
2026-02-26 00:02:38 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

---
+++ C++20/백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int K;
+    cin >> K;
+
+    stack<int> s;
+
+    for (int i = 0; i < K; i++) {
+        int num;
+        cin >> num;
+
+        if (num == 0) {
+            if (!s.empty()) s.pop();
+        } else {
+            s.push(num);
+        }
+    }
+
+    long long sum = 0;
+    while (!s.empty()) {
+        sum += s.top();
+        s.pop();
+    }
+
+    cout << sum << "\n";
+
+    return 0;
+}
