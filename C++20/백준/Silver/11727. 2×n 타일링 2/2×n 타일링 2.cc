d020ac4bef43ea82334f51fa222e5b2ece02dae8
Kim Donghyeok <109220011+Kim-DongHyeok@users.noreply.github.com>
2026-03-08 01:04:05 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int dp[1001];
+    dp[1] = 1;
+    dp[2] = 3;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+    }
+
+    cout << dp[n] << "\n";
+
+    return 0;
+}
