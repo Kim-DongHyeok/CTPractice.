7cabe458894b48a6e21391a2ad5e3530ebce0ece
Kim Donghyeok <109220011+Kim-DongHyeok@users.noreply.github.com>
2026-03-12 20:32:57 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+	long long arr[91];
+	arr[0] = 0;
+	arr[1] = 1;
+
+	int n;
+
+	cin >> n;
+
+	for (int i = 2; i <= n; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+
+	cout << arr[n];
+	
+}
